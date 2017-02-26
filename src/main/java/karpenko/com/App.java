187da5e11cbf9d15ddce0a11a99839e87eb7cb80
olegvyacheslavowich <20_ok_13@mail.ru>
2017-02-26 12:28:27 +0600

    OOP & XMLParsing

--- src/main/java/karpenko/com/App.java
+++ src/main/java/karpenko/com/App.java
@@ -1,10 +1,6 @@
 package karpenko.com;
 
-import karpenko.com.parsersfactory.impl.StAXFactory;
 import karpenko.com.starter.Start;
-import karpenko.com.xmlparsing.BouquetViewer;
-import karpenko.com.parsersfactory.ParsersFactory;
-import karpenko.com.parsersfactory.impl.SAXFactory;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.FileNotFoundException;

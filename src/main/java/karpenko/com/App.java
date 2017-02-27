d543cd4d2ecbe97f601ce55c92fdaa09919bd695
olegvyacheslavowich <20_ok_13@mail.ru>
2017-02-27 10:05:52 +0600

    OOP & XMLParsing

--- src/main/java/karpenko/com/App.java
+++ src/main/java/karpenko/com/App.java
@@ -4,15 +4,7 @@ import karpenko.com.starter.Start;
 import javax.xml.stream.XMLStreamException;
 import java.io.FileNotFoundException;
 
-;
 
-/**
- * Цветочница. Определить иерархию цветов. Создать несколько объек-
- * тов-цветов. Собрать букет (используя аксессуары) с определением его
- * стоимости. Провести сортировку цветов в букете на основе уровня све-
- * жести. Найти цветок в букете, соответствующий заданному диапазону
- * длин стеблей
- */
 public class App {
     public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
 

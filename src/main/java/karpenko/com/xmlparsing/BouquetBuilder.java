10dd9c9478ac472b755db65e7af7ff6514949d76
olegvyacheslavowich <20_ok_13@mail.ru>
2017-02-27 10:08:01 +0600

    OOP & XMLParsing

---
+++ src/main/java/karpenko/com/xmlparsing/BouquetBuilder.java
@@ -0,0 +1,11 @@
+package karpenko.com.xmlparsing;
+
+import karpenko.com.bouquet.Bouquet;
+
+/**
+ * Created by Олег on 27.02.2017.
+ */
+public interface BouquetBuilder {
+
+    Bouquet buildBouquet();
+}
